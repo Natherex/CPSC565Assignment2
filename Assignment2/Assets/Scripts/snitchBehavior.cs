a7b039053ae4376e269e653367c75ec558c5ddf2
Sammy Elrafih <sammy.elrafih1@ucalgary.ca>
2021-02-21 18:44:35 -0700

    Added the Snitch
    The snitch has been added and behavior for when colliding with a wall.

---
+++ Assignment2/Assets/Scripts/snitchBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snitchBehavior : MonoBehaviour
+{
+    private Rigidbody physicsBody;
+    Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        physicsBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector3(Random.Range(-100,100),Random.Range(-100,100),Random.Range(-100,100));
+        physicsBody.AddForce(direction);
+    }
+    void OnCollisionEnter ()
+    {
+        direction = -(direction*5);
+        physicsBody.AddForce(direction);
+    }
+}
