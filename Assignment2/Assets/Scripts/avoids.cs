55c52738afc3ce952eca0e24591bc254c27bcfda
Sammy Elrafih <sammy.elrafih1@ucalgary.ca>
2021-02-26 21:55:56 -0700

    Added Box Collider Triggers
    Added triggers around each layer so if it is triggered they are notified of other players and the arena itself.
    Current problem: at the begining the players avoid the floor and the walls but seem to steer towards the arena later on.

---
+++ Assignment2/Assets/Scripts/avoids.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class avoids : MonoBehaviour
+{
+    private Rigidbody physicsBody;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerBehavior player = gameObject.GetComponent<PlayerBehavior>();
+        physicsBody = GetComponent<Rigidbody>();
+        if(other.gameObject.tag == "Arena")
+        {
+    
+                Vector3 direction = ( other.transform.position - transform.position );
+                float distance = Vector3.Distance(transform.position, other.transform.position);
+
+                physicsBody.AddForce(direction * distance);
+                player.Exhaustion -= ((Mathf.Abs(physicsBody.velocity.x)) + (Mathf.Abs(physicsBody.velocity.x)) + (Mathf.Abs(physicsBody.velocity.x)) )* player.weight/50000;
+        }
+        else if(other.gameObject.name == "player(Clone)")
+        {
+                PlayerBehavior enemy = other.GetComponent<PlayerBehavior>();
+                if(enemy.team == player.team)
+                {
+                    float distance = Vector3.Distance(transform.position, other.transform.position);
+                    Vector3 direction = (transform.position - other.gameObject.transform.position);
+                    physicsBody.AddForce(direction * distance);
+                    player.Exhaustion -= ((Mathf.Abs(physicsBody.velocity.x)) + (Mathf.Abs(physicsBody.velocity.x)) + (Mathf.Abs(physicsBody.velocity.x)) )* player.weight/50000;
+                }else if(player.agro > enemy.agro && (player.Exhaustion > 10))
+                {
+                    float distance = Vector3.Distance(transform.position, other.transform.position);
+                    Vector3 direction = (other.gameObject.transform.position - transform.position);
+                    physicsBody.AddForce(direction * distance);
+                    player.Exhaustion -= ((Mathf.Abs(physicsBody.velocity.x)) + (Mathf.Abs(physicsBody.velocity.x)) + (Mathf.Abs(physicsBody.velocity.x)) )* player.weight/50000;
+                }else{
+                    float distance = Vector3.Distance(transform.position, other.transform.position);
+                    Vector3 direction = (transform.position - other.gameObject.transform.position);
+                    physicsBody.AddForce(direction * distance);
+                    player.Exhaustion -= ((Mathf.Abs(physicsBody.velocity.x)) + (Mathf.Abs(physicsBody.velocity.x)) + (Mathf.Abs(physicsBody.velocity.x)) )* player.weight/50000;
+                }
+        }
+    }
+
+}
